3f59ca9c9119b72d1030e5102889d7d7c1ad446d
Samir shano <name@email.com>
2026-09-26 13:50:07 +0200

    Add OpeningHours class with time calculations

---
+++ src/OpeningHours.js
@@ -0,0 +1,22 @@
+export class OpeningHours {
+  constructor(day, openTime, closeTime) {
+    this.day = day
+    this.openTime = openTime
+    this.closeTime = closeTime
+  }
+
+  isWithinOpeningHours(startTime, duration) {
+
+    const [openHours, openMinutes] = this.openTime.split(':').map(Number)
+    const [closeHours, closeMinutes] = this.closeTime.split(':').map(Number)
+
+    const openingTime = openHours * 60 + openMinutes
+    const closingTime = closeHours * 60 + closeMinutes
+
+    const [startHours, startMinutes] = startTime.split(':').map(Number)
+
+    const bookingStart = startHours * 60 + startMinutes
+    const bookingEnd = bookingStart + duration
+  }
+
+}
